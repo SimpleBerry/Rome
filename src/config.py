41f28978ce7a4ca6a84a0c07e634a78e1d9a30ae
Jianbo Wu <muyu.coder@outlook.com>
2024-11-25 05:23:25 +0000

    add mcts-llm

--- src/config.py
+++ src/config.py
@@ -1,4 +1,17 @@
+import dspy
+from mcts_llm.mctsr import MCTSr
+
+
 def create_mctsr():
+    ollama = dspy.OllamaLocal(
+        model="qwen2.5:7b-instruct",
+        model_type="chat",
+        temperature=1.0,
+        max_tokens=1024,
+        num_ctx=1024,
+        timeout_s=600
+    )
+    dspy.settings.configure(lm=ollama, experimental=True)
     return MCTSr()
 
 def create_tot():
