439c599732a63ce33f8b248784a57d905eea7610
Jianbo Wu <muyu.coder@outlook.com>
2024-12-09 05:23:30 +0000

    use Router

--- src/main.py
+++ src/main.py
@@ -3,7 +3,9 @@ from config import NAME2SOLVER
 from summarizer import Summarizer
 
 
+
 if __name__ == "__main__":
-    solver_name = "tot".lower()
-    solver = NAME2SOLVER[solver_name]
-    print(solver.solve("Is 9.11 larger than 9.9?")[0])
+    router = Router()
+    router.ensemble(['tot', 'mctsr'])
+    router.solve("Is 9.11 larger than 9.9?")
+    print(router.solutions)
--- src/router.py
+++ src/router.py
@@ -4,36 +4,27 @@ from config import NAME2SOLVER, Solver
 
 @dataclass
 class Router:
-
-    solver_pool: List[Tuple[str, Solver]]
-
-    # { problem : {solver name: solution} }
-    solutions: Dict[str, Dict[str, str]]
-
-    step_solutions: Dict[str, Dict[str, List[str]]]
-
-    # { problem : {solver name: grade} }
-    grades: Dict[str, Dict[str, float]]
-
-    def __init__(self):
-        self.solver_pool = []
-        self.solutions = {}
-        self.grades = {}
+    def __init__(self, solver_pool: List[Tuple[str, Solver]] = [], solutions: Dict[str, Dict[str, str]] = {}, grades: Dict[str, Dict[str, float]] = {}, step_solutions: Dict[str, Dict[str, List[str]]] = {}):
+        self.solver_pool = solver_pool  # solver_pool: List[Tuple[str, Solver]]
+        self.solutions = solutions      # solutions: { problem : {solver name: solution} }
+        self.step_solutions = step_solutions    # step_solutions: Dict[str, Dict[str, List[str]]]
+        self.grades = grades        # grades: { problem : {solver name: grade} }
 
     def route(self):
         pass
 
     def solve(self, problem: str) -> None:
         for name, solver in self.solver_pool:
-            # compatible with mcts-llm (https://github.com/NumberChiffre/mcts-llm)
-            self.solutions[problem][name] = solver.solve(problem)
+            solution = solver.solve(problem)
+            self.solutions.setdefault(problem, {})[name] = solution[0] if isinstance(solution, list) else solution
 
     def step_solve(self, problem: str, max_steps: int = 3) -> None:
         for name, solver in self.solver_pool:
-            self.step_solutions[problem][name] = solver.step_solve(problem, max_steps)
+            self.step_solutions.setdefault(problem, {})[name] = solver.step_solve(problem, max_steps)[0]
             
     def ensemble(self, names: List[str]) -> None:
         for name in names:
+            name = name.lower()
             if name not in NAME2SOLVER.keys():
                 raise ValueError(f"{name} is not a supported solver")
             self.solver_pool.append( (name, NAME2SOLVER[name]) )
